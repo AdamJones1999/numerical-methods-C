c421bbbb9ff3a7c4447b332258b592aaa8f4b8ef
agj171 <agj171@mail.usask.ca>
2025-12-02 04:30:21 -0600

    euler_method() changes: fixed 2nd param type in x_prime() type decl to float * for multi var array compatibility. Added 3rd param type uint to x_prime() for number of dep vars. Added number of dep vars N_x to param list.

--- euler_method.c
+++ euler_method.c
@@ -1,11 +1,12 @@
 #include <stdlib.h>
 #include <ctype.h>
+#include "nummethods.h"
 
-float *euler_method(float (*x_prime)(float, float), float x[], float t[], float dt, uint N) {
+float *euler_method(float (*x_prime)(float, float *, uint), float x[], float t[], float dt, uint N_t, uint N_x) {
 	uint i;
 	// compute solution values up to index N
-	for (i=0; i<N-1; i++) {
-		x[i+1] = x[i] + x_prime(t[i], x[i]) * dt;
+	for (i=0; i<N_t-1; i++) {
+		x[i+1] = x[i] + x_prime(t[i], &x[i], N_x) * dt;
 	} 
 	return x;	
 }
