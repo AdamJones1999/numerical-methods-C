eedc83bc520238a4e2b9b8325ff90cafc8afacb4
agj171 <agj171@mail.usask.ca>
2025-12-03 18:02:47 -0600

    both: changed nummethod.h to <> enclosed to indicate system header. euler_method.c: added start of to_csv() implementation.

--- euler_method.c
+++ euler_method.c
@@ -1,6 +1,6 @@
 #include <stdlib.h>
 #include <ctype.h>
-#include "nummethods.h"
+#include <nummethods.h>
 
 float *euler_method(float (*x_prime)(float, float *, uint), float x[], float t[], float dt, uint N_t, uint N_x) {
 	uint i;
@@ -9,4 +9,26 @@ float *euler_method(float (*x_prime)(float, float *, uint), float x[], float t[]
 		x[i+1] = x[i] + x_prime(t[i], &x[i], N_x) * dt;
 	} 
 	return x;	
+}
+
+int to_csv(float *, dims, char name[]) {
+	FILE *fp;
+	if ((fp = fopen(name, 'a')) == NULL) {
+		printf("cannot open file: %s", name);
+	}
+	else {
+		if (dims == 1) {
+
+		}
+		else if (dims == 2) {
+			printf("support for writing 2D data is coming soon"\n);
+			return -1;
+		}
+		else {
+			printf("writing %d dimension data to a .csv not supported.\n");
+			return -1;
+		}
+	}
+	fclose(FILE *fp);
+	return 0;
 }
