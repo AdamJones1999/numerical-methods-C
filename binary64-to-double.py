03f31b40e8b2caf21fa822a0014113b9f557f1f0
agj171 <agj171@mail.usask.ca>
2025-12-08 03:29:48 -0600

    renamed c-to-pyplot to binary64-to-double.py and added loading and unpacking binary64 formatted data to type double data.

---
+++ binary64-to-double.py
@@ -0,0 +1,33 @@
+'''
+OS: Ubuntu Linux
+Script which does the following:
+1. Read little endian binary data output from 
+numerical methods in c into python to plot.
+2. Convert binary64 in memoryview to double.
+'''
+import os
+import struct
+
+datapath = "../numerical-methods-C/test.data"
+size_bytes = os.path.getsize(datapath)
+
+DOUBLE_SIZE = 8
+
+with open(datapath, 'rb') as f: # "read binary" mode 
+	buffer = bytearray(size_bytes)
+	view = memoryview(buffer)
+	nbytes = f.readinto(view) # binary mode required for this to work
+	print(f"size of data in bytes: {size_bytes}")
+	print(f"hex dump of data: {buffer.hex()}")
+
+
+# converting binary64 to floating point using structs
+n_doub = nbytes // DOUBLE_SIZE
+print(f"n_doub: {n_doub}")
+
+st = struct.Struct(f'<{5}d')
+double_arr = bytearray(n_doub)
+conv_view = st.unpack(view)
+print(f"converted double values: {conv_view}")
+
+f.close()
