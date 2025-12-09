44b76c20d3be44000556d9ad166a467fa274275f
agj171 <agj171@mail.usask.ca>
2025-12-08 23:36:53 -0600

    reads and unpacks binary64 data to separate dep and indep var data and plot 1 indep and 1 dep var on a graph

--- binary64-to-numpy-array.py
+++ binary64-to-numpy-array.py
@@ -1,7 +1,23 @@
 import numpy as np
 
-fp = "test.data"
+from matplotlib import pyplot as plt
+fp = "data/test.data"
 
-data_arr = np.fromfile(fp, dtype=np.float64)
+data_1d = np.fromfile(fp, dtype=np.float64)
 
-print(data_arr)
+data_2d = data_1d.reshape((2, 5))
+t = data_2d[0]
+r = data_2d[1]
+
+print("t: ")
+print(t)
+print("r: ")
+print(r)
+# ======== plotting ========
+plt.plot(t, r)
+plt.show()
+
+'''
+plt.plot(r, )
+ax
+'''
