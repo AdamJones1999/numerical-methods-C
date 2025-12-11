14edab937f972336608d538115f71f0e08f72910
agj171 <agj171@mail.usask.ca>
2025-12-10 22:14:43 -0600

    added shoODE_dydt() and shoODE_dydt2() funcs for the simple harmonic oscillator solver test. Added to but did not finish test2() which uses aforementioned ODE functions. Also cleaned up the code for writing data to files.

--- euler_test.c
+++ euler_test.c
@@ -16,6 +16,7 @@ to arrays as arguments.
 	k: calculated dr/dt.
 */
 double simpleODE1(double t, double r[], uint N_rvars) {
+	(void) t;
 	double k; // dr/dt
 	(void)t; // cast to void to suppresses unused param warning
 	if (N_rvars != 1) {
@@ -28,23 +29,36 @@ double simpleODE1(double t, double r[], uint N_rvars) {
 	}
 }
 
-double smhODE(double t, double r[], uint N_rvars) {
+// slope calc for y'
+double shoODE_dydt(double t, double y[], uint N_rvars) {
 	(void) t;
-	(void) r;
-	(void) N_rvars;
-	double drdt = 0; // slope
+	double dydt ; // 1st deriv of y
 	if (N_rvars != 1) {
 		fprintf(stderr, "ERROR: Given %d dependent var values, should be 1.", N_rvars);
 		exit(1);
 	}
 	else {
-		;
-		return drdt;
+		dydt = y[0];
+		return dydt;
+	}
+}
+
+// slope calc for y''
+double shoODE_dydt2(double t, double y[], uint N_rvars) {
+	(void) t;
+	double dydt2; // 2nd deriv of y
+	if (N_rvars != 1) {
+		fprintf(stderr, "ERROR: Given %d dependent var values, should be 1.", N_rvars);
+		exit(1);
+	}
+	else {
+		dydt2 = -16*y[0];
+		return dydt2;
 	}
 }
 
 // //////// test 1: simpleODE1 ////////  
-void test1() {
+int test1() {
 	char *fn = "data/test1.data";
 	uint N_t = 5;
 	uint N_rvars = 1; // num dependent variables
@@ -65,35 +79,55 @@ void test1() {
 	euler_method(simpleODE1, r, t, dt, N_t, N_rvars);
 	printf("%d element solution r: \n{ %f, %f, %f, %f, %f }\n", N_t, r[0], r[1], r[2], r[3], r[4]);
 
-	if (write_to_bin(t, N_t, NDIMS, fn) == 0) {
-		if (write_to_bin(r, N_rvars * N_t, NDIMS, fn) == 0) {
-			free(t);
-			free(r);
-		}
+	if (write_to_bin(t, N_t, NDIMS, fn) == 0 && \
+		write_to_bin(r, N_rvars * N_t, NDIMS, fn) == 0) {
+		free(t);
+		free(r);
+		return 0;
 	}
 	else {
 		printf("writing to %s failed\n", fn);
+		return -1;
 	}
 }
 
-void test2() {
+int test2() {
 	// //////// test 2: simple harmonic motion (smhODE) ////////  
 	char *fn = "data/test2.data";
+	uint NDIMS = 1;
 	uint N_t = 1000;
-	uint N_rvars = 1;
+	uint N_yvars = 1;
 	double *t = (double *) malloc(N_t * sizeof(double));
-	double *r = (double *) malloc(N_rvars * N_t * sizeof(double));
-	double *drdt = (double *) malloc(N_rvars * N_t * sizeof(double));
+	double *y = (double *) malloc(N_yvars * N_t * sizeof(double));
+	// double *dydt = (double *) malloc(N_yvars * N_t * sizeof(double));
+	// initial conditions
+	y[0] = 1;
+	double dydt_curr = 1;
+	double dydt_next = 0;
 	double dt = 0.01;
-	(void) drdt;
-	(void) fn;
-
-	uint i;
 	t[0] = 0;
-	for (i=1; i<N_t; i++) {
-		t[i] = t[i-1] + dt;
+	uint i;
+	for (i=1; i<N_t; i++) { // init indep var array
+		t[i] = t[i-1]+dt;
 	}
-	r[0] = 0;
+	// numerical solving loop
+	for (i=0; i<N_t-1; i++) {
+		y[i+1] = euler_single(shoODE_dydt, y[i], t[i], dt, N_yvars);
+		dydt_next = euler_single( \
+			shoODE_dydt2, dydt_curr, t[i], dt, N_yvars); // calc dydt[i+1]
+		dydt_curr = dydt_next;
+	}
+
+	// write output to file
+	if (write_to_bin(t, N_t, NDIMS, fn) == 0 && write_to_bin(y, N_t, NDIMS, fn) == 0) {
+		free(t);
+		free(y);
+		return 0;
+	}
+	else { 
+		printf("writing to %s failed\n", fn);
+		return -1;
+	}
 }
 
 int main() {
@@ -103,6 +137,7 @@ int main() {
 	// var decl and init
 	fprintf(stdout, "IN MAIN\n\n");
 	test1();
+	test2();
 
 	// //////////////// END TESING ////////////////
 	return 0;
