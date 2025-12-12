9ac3a004ccc9c46bae3acdd233afc2242b16f1b2
agj171 <agj171@mail.usask.ca>
2025-12-12 16:53:50 -0600

    added smhODE error plot script. Touched up binary-to-np-array.py

--- binary64-to-np-array.py
+++ binary64-to-np-array.py
@@ -2,27 +2,17 @@ import numpy as np
 import sys
 from matplotlib import pyplot as plt
 
-#fp = "data/test1.data"
+# read in data and dimensions
 fp = sys.argv[1] #
 rows = int(sys.argv[2]) # rows in data file
 cols = int(sys.argv[3]) # columns data file
-print(fp)
-
 data_1d = np.fromfile(fp, dtype=np.float64)
 
+# organize data into np arrays
 data_2d = data_1d.reshape((rows, cols))
 t = data_2d[0] # indep var 
 r = data_2d[1] # dep var
 
-print("t: ")
-print(t)
-print("r: ")
-print(r)
-# ======== plotting ========
+# plotting 
 plt.plot(t, r)
 plt.show()
-
-'''
-plt.plot(r, )
-ax
-'''
