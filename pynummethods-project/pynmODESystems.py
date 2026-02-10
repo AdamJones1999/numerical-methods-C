464876e7d4c6aaae9c83124413b9a57e1ec5ed60
agj171 <agj171@mail.usask.ca>
2026-02-09 18:31:48 -0600

    fixed err msg typo

--- pynummethods-project/pynmODESystems.py
+++ pynummethods-project/pynmODESystems.py
@@ -7,7 +7,7 @@ They are to be used within numerical methods defined in pynummethods.py
 import numpy as np
 
 def Nvars_err_msg(f_name: str, Nvars: int, Nvars_req: int) -> str:
-	return f"{f_name} requires {Nvars_req} dep vars. Provided {Nvars}/n"
+	return f"{f_name} requires {Nvars_req} dep vars. Provided {Nvars}"
 
 def non_linear_osc(t: float, rcol: np.ndarray, Nr: int) -> np.ndarray:
 	Nr_req = 2
