b69423252987433d6d733b98ecf1d9e383735471
agj171 <agj171@mail.usask.ca>
2026-02-10 15:46:01 -0600

    nummethods.h: fixing from merging multivar into rk4. Had declarations that were for the same function with different types so both got carried over in the merge.

--- nummethods.h
+++ nummethods.h
@@ -78,15 +78,6 @@ double **midpoint_single(void (*drdt_f)(double *, double *, double, uint), \
 	double t, double **r, uint j_r, double dt, uint Nr);
 
 
-//currently only single var
-void rk4_single(double (*drdt)(double, double *, double *, uint), \
-	double t, double r[], double drdt_args[], double dt, uint N_r);
-
-//currently only single var tested
-double *midpoint_single(double (*drdt)(double, double *, double *, uint), \
-	double t, double r[], double drdt_args[], double dt, uint N_r);
-
-
 /*
 @description:
 	currently only single var!!
