Fixed passing ndarray 't' rather than float 't[i]' to numerical method in all current tests. Added type annotations to all but the plotting functions in pynm-test.py to prevent such a type discrepency in the future.

pynummethods-project/pynm-test.py
@@ -1,10 +1,11 @@
 import numpy as np
 import pynmODESystems as odes
 from matplotlib import pyplot  as plt
+from typing import Tuple
 import pynummethods as nm
 
 
-def shmODEEulerTest():
+def shmODEEulerTest() -> Tuple[np.ndarray, np.ndarray, float, int]:
 	t0 = 0
 	tf = 10
 	dt = 0.001
@@ -18,11 +19,11 @@ def shmODEEulerTest():
 	print(f"Nt shape: {Nt}")
 	r[:,0] = r0
 	for i in range(0,Nt-1):
-		nm.euler_single(odes.shm, t, r, i, dt, Nr)
+		nm.euler_single(odes.shm, t[i], r, i, dt, Nr)
 	print(f"shmODEEulerTest first few: {r[0,0:5]}")
 	return t, r, dt, Nt
 
-def shmODEMidpointTest():
+def shmODEMidpointTest() -> Tuple[np.ndarray, np.ndarray, float, int]:
 	t0 = 0
 	tf = 10
 	dt = 0.001
@@ -36,12 +37,12 @@ def shmODEMidpointTest():
 	print(f"Nt shape: {Nt}")
 	r[:,0] = r0
 	for i in range(0,Nt-1):
-		nm.midpoint_single(odes.shm, t, r, i, dt, Nr)
+		nm.midpoint_single(odes.shm, t[i], r, i, dt, Nr)
 	print(f"shmODEMidpointTest first few: {r[0,0:5]}")
 	return t, r, dt, Nt
 
 
-def rossler_euler_test():
+def rossler_euler_test() -> Tuple[np.ndarray, np.ndarray, float, int]:
 	t0 = 0
 	dt = 0.1
 	tf = 500+dt
@@ -52,10 +53,10 @@ def rossler_euler_test():
 	r0 = [0, 1, 0]
 	r[:, 0] = r0
 	for i in range(0, Nt-1):
-		nm.euler_single(odes.rossler, t, r, i, dt, Nr)
+		nm.euler_single(odes.rossler, t[i], r, i, dt, Nr)
 	return t, r, dt, Nt
 
-def non_linear_osc_rk4_test():
+def non_linear_osc_rk4_test() -> Tuple[np.ndarray, np.ndarray, float, int]:
 	t0 = 0.1
 	dt = 0.01
 	tf = 30+dt
@@ -66,24 +67,12 @@ def non_linear_osc_rk4_test():
 	r0 = [0.0099983333, 0.19999]
 	r[:, 0] = r0
 	for i in range(0, Nt-1):
-		nm.rk4_single(odes.non_linear_osc, t, r, i, dt, Nr)
-	return t, r, dt, Nt, k
-
-def non_linear_osc_rk4_plot(t, r, dt, Nt, k):
-
-	# analytical soln
-	analytical_soln = np.sin(np.sqrt(k)*t^2)
-
-	plt.figure(figsize=(16, 8))
-	plt.tight_layout()
-	plt.subplot(2, 1, 1)
-	plt.plot(t, r[0, :] - analytical_soln)
-	plt.title(f"non_linear_osc using rk4: x-$x_{analytical}$ over t=[{t_e1[0]}, {t_e1[Nt_e1-1]}], dt={dt_e1}")
-
+		nm.rk4_single(odes.non_linear_osc, t[i], r, i, dt, Nr)
+	return t, r, dt, Nt
 
 def shm_euler_midpoint_plot(t_e1, r_e1, dt_e1, Nt_e1, t_m1, r_m1, dt_m1, Nt_m1):
 	# ---------- analytical solution to y'' + 16y = 0 ----------
-	r_shm_analytical = np.cos(4*t_e1) + 0.25*np.sin(4*t_e1)
+	x_shm_analytical = np.cos(4*t_e1) + 0.25*np.sin(4*t_e1)
 
 	# euler method
 	plt.figure(figsize=(16, 8))
@@ -93,8 +82,10 @@ def shm_euler_midpoint_plot(t_e1, r_e1, dt_e1, Nt_e1, t_m1, r_m1, dt_m1, Nt_m1):
 	plt.title(f"euler method shmODE r over t=[{t_e1[0]}, {t_e1[Nt_e1-1]}], dt={dt_e1}")
 
 	plt.subplot(2, 2, 2)
-	plt.plot(t_e1, r_e1[0, :] - r_shm_analytical)
-	plt.title(f"euler method shmODE r-r_analytical over t=[{t_e1[0]}, {t_e1[Nt_e1-1]}], dt={dt_e1}")
+	plt.plot(t_e1, r_e1[0, :] - x_shm_analytical)
+	plt.title(f"euler method shmODE error over t=[{t_e1[0]}, {t_e1[Nt_e1-1]}], dt={dt_e1}")
+	plt.ylabel("x-x_analytical")
+	plt.xlabel("t")
 
 	# midpoint method
 	plt.subplot(2, 2, 3)
@@ -102,7 +93,7 @@ def shm_euler_midpoint_plot(t_e1, r_e1, dt_e1, Nt_e1, t_m1, r_m1, dt_m1, Nt_m1):
 	plt.title(f"midpoint method shmODE using r over t=[{t_m1[0]}, {t_m1[Nt_m1-1]}], dt={dt_m1}")
 	
 	plt.subplot(2, 2, 4)
-	plt.plot(t_m1, r_m1[0, :] - r_shm_analytical)
+	plt.plot(t_m1, r_m1[0, :] - x_shm_analytical)
 	plt.title(f"midpoint method shmODE r-r_analytical over t=[{t_m1[0]}, {t_m1[Nt_m1-1]}], dt={dt_m1}")
 	
 	plt.show()
@@ -152,24 +143,34 @@ def rossler_euler_plot(t, r, dt, Nt):
 
 	plt.show()
 
-def non_linear_osc_plot():
-	return
+def non_linear_osc_rk4_plot(t, r, dt, Nt):
+
+	# analytical soln
+	k = 1 # MUST MATCH NUMERICAL SOLN k
+	analytical_soln = np.sin(np.sqrt(k) * t ** 2)
+
+	plt.figure(figsize=(16, 8))
+	plt.tight_layout()
+	plt.subplot(2, 1, 1)
+	plt.plot(t, abs(r[0, :] - analytical_soln))
+	plt.title(f"non_linear_osc using rk4: x-x_analytical over t=[{t[0]}, {t[Nt-1]}], dt={dt}")
+	plt.ylabel("abs(r-r_analytical)")
+	plt.xlabel("t")
+
+	plt.show()
 
 
 if __name__=="__main__":
-	# simple harmonic oscillator ODE test
 	'''
+	# simple harmonic oscillator ODE test
 	t_e1, r_e1, dt_e1, Nt_e1 = shmODEEulerTest()
 	t_m1, r_m1, dt_m1, Nt_m1 = shmODEMidpointTest()
 	shm_euler_midpoint_plot(t_e1, r_e1, dt_e1, Nt_e1, t_m1, r_m1, dt_m1, Nt_m1)
-	'''
 
 	# rossler ODE system test
-	'''
 	t_e_r1, r_e_r1, dt_e_r1, Nt_e_r1 = rossler_euler_test()
 	rossler_euler_plot(t_e_r1, r_e_r1, dt_e_r1, Nt_e_r1)
 	'''
-
 	# non linear oscillator using rk4 test
-	t_rk4_1, r_rk4_1, dt_rk4_1, Nt_rk4_1, k_rk4_1 = non_linear_osc_rk4_test()
-	non_linear_osc_rk4_plot(t_rk4_1, r_rk4_1, dt_rk4_1, Nt_rk4_1, k_rk4_1)
+	t_rk4_1, r_rk4_1, dt_rk4_1, Nt_rk4_1 = non_linear_osc_rk4_test()
+	non_linear_osc_rk4_plot(t_rk4_1, r_rk4_1, dt_rk4_1, Nt_rk4_1)