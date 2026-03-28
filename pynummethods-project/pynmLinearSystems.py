510df6a3000a7fc16b449368d26dbaf956aa054c
agj171 <agj171@mail.usask.ca>
2026-03-27 18:16:01 -0600

    pynmLinearSystems.py: added nvars var

--- pynummethods-project/pynmLinearSystems.py
+++ pynummethods-project/pynmLinearSystems.py
@@ -8,11 +8,11 @@ import numpy as np
 
 """ODE system for testing newton rhapson multivar method"""
 def NR_test_ls(f_r: np.ndarray, r: np.ndarray, Nr: int):
-	if (f_r.shape[0] != 2):
-		print("error, should be 2 rows")
+	nvars = 2
+	if (not (f_r.shape[0] == r.shape[0] == Nr) and Nr != 2):
+		print(f"ERROR: # of elements in f_r and r must equal Nr and Nr must = {nvars}")
 	x1 = r[0]
 	x2 = r[1]
 	f_r[0] = x2 * (1 + x1**2) - np.exp(-np.exp(x1+x2))
 	f_r[1] = -x2 * np.sin(x1) + 0.5 - x1 * np.cos(x2)
-
 	return
